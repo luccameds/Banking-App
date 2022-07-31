9d4fe2f1031ad88c6917a7c89576651a05624b3e
Lucca Meds <luccameds@gmail.com>
2022-07-31 13:14:49 -0300

    Updated the App Theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { ApplicationProvider, Layout } from "@ui-kitten/components";
+
+import * as eva from "@eva-design/eva";
 
 import OnboardingScreen from "./src/pages/OnboardingScreen";
 
-const App = () => {
-  return <OnboardingScreen />;
-};
-
-export default App;
+export default () => (
+  <ApplicationProvider {...eva} theme={eva["light"]}>
+    <StatusBar />
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <OnboardingScreen />
+    </Layout>
+  </ApplicationProvider>
+);
